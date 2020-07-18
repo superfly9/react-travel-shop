316ff4af2a977381fc252efa2c34a8b71b6e8fc1
unknown <superfly09@daum.net>
2020-07-18 19:31:38 +0900

    [FEAT] 랜딩페이지 접속시 DB에서 데이터 불러와서 카드 렌더링/더보기 버튼을 누르면 DB에서 limit으로 설정한 갯수 이내에서  데이터들을 더 불러와 렌더링하는 기능 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios';
+import './LandingPage.css';
 
 function LandingPage() {
+    const [Products,setProducts]=useState([]);
+    const [Skip,SetSkip]=useState(0);
+    const [Limit,SetLimit]=useState(8);
+    const [ProductInfoLength,SetProductInfoLength]=useState(0);
+    useEffect(()=>{
+        let body = {
+            skip : Skip,
+            limit :Limit
+        }
+        getProducts(body);
+    },[]);
+    const getProducts =(body)=>{
+        Axios.post('/api/product/products',body).then(response=>{
+            if (response.data.success) {
+                //이거 불필요하다 생각
+                if (body.loadMore) {
+                    setProducts([...Products,...response.data.productInfo]);
+                } else {
+                    //처음으로 데이터를 load해올 때
+                    setProducts([...Products,...response.data.productInfo]);
+                }
+            } else { 
+                alert('상품정보를 가져오는 데 실패했습니다.')
+            }
+            SetProductInfoLength(response.data.productInfoLength);
+        })
+    }
+    const loadMoreHandler = ()=>{
+        let skip = Skip + Limit;
+        let body={
+            skip,
+            limit:Limit,
+            loadMore : true
+        }
+        getProducts(body);
+        SetSkip(skip);
+    }
+    const renderCard = Products.map((item,index)=>{
+        console.log(item)
+      return (
+        <div key={index} className='card'>
+            <img className='product_img' src={`http://localhost:5000/${item.images[0]}`} />
+            <span className='product_description'>{item.description}</span>
+            <span className='product_price'>{item.price}</span>
+        </div>
+      )  
+    })
     return (
-     <div>
-         
+        // flex
+     <div className='landingpage_container'>
+        <h2 className='landingpage_title'>Let's travel Anywhere</h2>  
+            {/* css grid */}
+        <div className='card_container'>
+            {renderCard}
+        </div>
+        <div className='btn_container'>
+            {ProductInfoLength>Limit&&<button onClick={loadMoreHandler}>더보기</button>}
+        </div>
      </div>
     )
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -21,6 +21,7 @@ productRouter.post('/image',upload,(req,res)=>{
     } 
     res.json({success:false});
 })
+// 업로드 페이지에서 전달하는 상품의 정보들을 저장
 productRouter.post('/save',async (req,res)=>{
   console.log('req.body:',req.body)
   const product=await new Product(req.body);
@@ -30,5 +31,27 @@ productRouter.post('/save',async (req,res)=>{
     res.json({success:true})
   })
 })
+// DB에 저장된 상품 정보들을 불러옴
+productRouter.post('/products',async (req,res)=>{
+  
+  let {limit,skip} = req.body;
+  console.log(limit,skip,'for more Loading');
+  limit = limit ? parseInt(limit) : 20;
+  skip = skip ? parseInt(skip) : 0;
+  await Product.find({})
+    .populate('writer')
+    .skip(skip)
+    .limit(limit)
+    .exec((err,productInfo)=>{
+      if (err) return res.json({success:false,err})
+      res.json({
+        success:true,
+        productInfo,
+        productInfoLength:productInfo.length
+      })
+    })
+})
+
+
 
 module.exports = productRouter;

9fdc65d9471e78265f53fc496625db9812d9512a
unknown <superfly09@daum.net>
2020-08-22 01:19:14 +0900

    feat: 로그인했을때만 상품을 카트에 추가할 수 있게 함

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import {useDispatch} from 'react-redux';
+import React from 'react';
+import {useDispatch,useSelector} from 'react-redux';
 import { addToCart } from '../../../../_actions/user_actions';
 
 const ProductInfo=(props)=>{
     const dispatch = useDispatch();
+    const user = useSelector(state => state.user);
+    const {userData} = user;
+    const userId = userData ? userData._id : null
+    console.log(userData,userId)
     const clickAddToCart = (e)=>{
-        console.log('at ProductInfo:',props.detail)
         dispatch(addToCart(props.detail._id))
             .then(response=>{
-                console.log('data from redux',response.payload);
             })            
     }
     return (
@@ -22,7 +24,11 @@ const ProductInfo=(props)=>{
             <p className='item_description'>
                 {props.detail.description}
             </p>
-            <button className='add_cart_btn' onClick={clickAddToCart}>Add to Cart</button>
+            {userId ?
+                <button className='add_cart_btn' onClick={clickAddToCart}>Add to Cart</button>
+                :
+                <p>카트에 추가하려면 로그인을 하세요</p>
+            }
         </div>
     )
 }
